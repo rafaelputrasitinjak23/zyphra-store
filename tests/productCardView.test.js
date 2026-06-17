623d33daed4e261fd9904da4122d44c913f66e1a
rafaelputra23 <rafaeltinjak8@gmail.com>
2026-06-17 09:59:53 +0700

    Add files via upload

---
+++ tests/productCardView.test.js
@@ -0,0 +1,16 @@
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const template = fs.readFileSync(
+  path.join(__dirname, '..', 'views', 'partials', 'product-card.ejs'),
+  'utf8'
+);
+
+test('kartu produk selalu menampilkan nama dan harga script', () => {
+  assert.match(template, /Nama script/);
+  assert.match(template, /Harga script/);
+  assert.match(template, /product\.name/);
+  assert.match(template, /rupiah\(product\.(?:promoPrice|price)/);
+});
